5b3a12659b8eb31f5edb0066ce6992b004fdafeb
Unknown <sgforba@gmail.com>
2017-09-30 17:59:56 -0400

    +

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -1,18 +1,46 @@
 var webshot = require("webshot");
+var fs = require('fs');
+var storage = require('@google-cloud/storage');
 
+
+//Google Cloud
+var gcs = storage({
+  projectId: 'email-library',
+  keyFilename: 'keyfile.json'
+});
+
+var bucket = gcs.bucket('test-bucket-1989');
 
 
 //Webshot Stuff
 var options = {
 	windowSize: { width: 1024 , height: 768 },
-	shotSize: {width: 'window', height: 'window'}
+	shotSize: {width: 'window', height: 'window'},
+	onResourceReceived: function(response) {
+  		console.log("TEST");
+	}
+
 }
 module.exports.screenshot = function(a, b) {
-	var filePath = './public/images/screenshots/'+b
-	webshot(a, filePath, options, function(err) {
- 		console.log("Saved!")
-	});
 
+	var options = {
+		windowSize: { width: 500 , height: 500 },
+		shotSize: {width: 'window', height: 'window'},
+		onResourceReceived: function(response) {
+	  		console.log("SCREENSHOT CONMPLETE");
+		}
+
+	}
+
+	var filename = './public/images/screenshots/'+b;
+
+
+
+	webshot(a, filename, options, function(err) {
+ 		if (err) {
+ 			console.log(er)
+ 		} 
+	});
 }
 
-//GridFS Stuff
+
